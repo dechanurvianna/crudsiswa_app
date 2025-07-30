08bbb2d39539b8e912548fe5425c7804aa8518ac
dechanurvianna <dechanurvianna@gmail.com>
2025-07-30 14:59:13 +0700

    menambahkan class dan mengubah model user

---
+++ crud-siswa/app/Models/Clas.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Clas extends Model
+{
+    protected $table = "Clases";
+
+    protected $guarde =[];
+
+    public function users (){
+        return $this->hasMany(User::clases, 'clas_id');
+    }
+}

06c168c451cafcdaec67078be1e5f4aed1775656
dechanurvianna <dechanurvianna@gmail.com>
2025-07-31 16:17:25 +0700

    menambahkan view siswa

---
+++ crud-siswa/resources/views/siswa/index.blade.php
@@ -0,0 +1,38 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Baru</title>
+</head>
+<body>
+        <h1>Halaman Baru</h1>
+        <h1>List Data</h1>
+        <a href="siswa/create">Tambah</a>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Foto</th>
+                    <th>Name</th>
+                    <th>Alamat</th>
+                    <th>Kelas</th>
+                    <th>option</th>
+</tr>
+</thead>
+</body>
+    <tr>
+        <td>Fot.jpg</td>
+        <td>Decha</td>
+        <td>XI PPLG 2</td>
+        <td>Jl. abcd</td>
+        <td class="option-likns">
+            <a href="#">Hapus</a>
+            <a href="#">Edit</a>
+            <a href="#">Detail</a>
+</td>
+</td>
+<tbody>
+</table>
+</div>
+</body>
+</html>
